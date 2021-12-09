73466aa5ffab66980981132fde9c3bb3046a3419
Nicolas Conte <nicoconte1999@gmail.com>
2021-12-08 21:56:19 -0300

    .

--- Services/AppService.cs
+++ Services/AppService.cs
@@ -13,23 +13,6 @@ namespace RetroClient.Services
 	public class AppService
 	{
 
-        public bool IsUserPlaying { get; private set; } = false;
-        public string ActiveGameName { get; private set; } = string.Empty;
-
-        public event Action OnUserPlayStateChange;
-
-        public void SetUserPlayState(bool newState, string currentGameName)
-        {
-            IsUserPlaying = newState;
-            ActiveGameName = currentGameName;
-            NotifyStateChanged();
-        } 
-
-        private void NotifyStateChanged() 
-        {
-            OnUserPlayStateChange?.Invoke();
-        }
-
         public static void Close(Action beforeExitAction=null)
         {
             if (beforeExitAction == null)
